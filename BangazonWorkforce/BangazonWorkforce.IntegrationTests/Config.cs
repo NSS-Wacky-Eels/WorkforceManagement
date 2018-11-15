feea82af2d51a1c51db4aeecea513698c7665a46
Taylor Gulley <taylorlgulley@yahoo.com>
2018-11-15 15:34:09 -0600

    removed Config fromn cahce

--- BangazonWorkforce/BangazonWorkforce.IntegrationTests/Config.cs
+++ BangazonWorkforce/BangazonWorkforce.IntegrationTests/Config.cs
@@ -6,7 +6,7 @@
         {
             get
             {
-                return "__PLACE CONNECTION STRING HERE__";
+                return "Server=DESKTOP-GO8BVSM\SQLEXPRESS;Initial Catalog=BangazonAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             }
         }
     }
