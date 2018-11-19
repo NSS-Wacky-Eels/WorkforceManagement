e4531328cadb1dc78822c12162dd92d1b7d02d3e
Taylor Gulley <taylorlgulley@yahoo.com>
2018-11-19 11:35:13 -0600

    changed the view model of Edit

--- BangazonWorkforce/BangazonWorkforce/Models/ViewModels/EmployeeEditViewModel.cs
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/EmployeeEditViewModel.cs
@@ -13,55 +13,86 @@ namespace BangazonWorkforce.Models.ViewModels
 {
     public class EmployeeEditViewModel
     {
-        private readonly IConfiguration _config;
 
         public Employee Employee { get; set; }
 
-        public List<SelectListItem> Computers { get; set; }
+        // List of all computer to select new computer
+        public List<Computer> AllComputers { get; set; }
+        //Currently assigned computer
+        public Computer Computer { get; set; }
 
-        public List<SelectListItem> Departments { get; set; }
+        public List<Department> AllDepartments { get; set; }
+        //List of all training programs
+        public List<TrainingProgram> AllTrainingPrograms { get; set; }
+        //List of the training programs the employee has chosen
+        public List<TrainingProgram> EmployeeChosenTrainingPrograms { get; set; }
+        // List of the selected Training Programs Ids
+        public List<int> SelectedTrainingProgramsIds { get; set; }
+        // List of already chosen Training Programs Ids by the employee
+        public List<int> PreSelectedTrainingPrograms { get; set; }
 
-        public List<SelectListItem> TrainingPrograms { get; set; }
+        //public EmployeeEditViewModel() { }
 
-        public EmployeeEditViewModel() { }
-
-        public EmployeeEditViewModel(IConfiguration config)
+        public List<SelectListItem> AllDepartmentOptions
         {
-
-            using (IDbConnection conn = new SqlConnection(config.GetConnectionString("DefaultConnection")))
+            get
             {
-                Computers = conn.Query<Computer>(@"
-                    SELECT Id, Name FROM Computer;
-                ")
-                .Select(li => new SelectListItem
+                if (AllDepartments == null)
                 {
-                    Text = li.Make,
-                    Value = li.Id.ToString()
-                }).ToList();
-                ;
-                Computers = conn.Query<Department>(@"
-                    SELECT Id, Name FROM Department;
-                ")
-                .Select(li => new SelectListItem
-                {
-                    Text = li.Name,
-                    Value = li.Id.ToString()
-                }).ToList();
-                ;
-                Computers = conn.Query<TrainingProgram>(@"
-                    SELECT Id, 
-                            Name,
-                            StartDate
-                     FROM TrainingProgram tp
-                     WHERE StartDate >= CONVERT(DATETIME, {fn CURDATE()});
-                ")
-                .Select(li => new SelectListItem
-                {
-                    Text = li.Name,
-                    Value = li.Id.ToString()
-                }).ToList();
-                ;
+                    return null;
+                }
+
+                return AllDepartments
+                    .Select((d) => new SelectListItem(d.Name, d.Id.ToString()))
+                    .ToList();
             }
         }
+
+        public List<SelectListItem> AllComputerOptions
+        {
+            get
+            {
+                if (AllComputers == null)
+                {
+                    return null;
+                }
+                // Makes a new empty list 
+                List<SelectListItem> ComputerOptions = new List<SelectListItem>();
+                // Sets the data in the list to hold only Makes and Ids
+                ComputerOptions = AllComputers
+                    .Select((c) => new SelectListItem(c.Make, c.Id.ToString()))
+                    .ToList();
+                // If the Employee doesn't have a computer assigned then set position 0 to Assign a Computer
+                if (Computer.Id == 0)
+                {
+                    ComputerOptions.Insert(0, new SelectListItem
+                    {
+                        Text = "Assign a Computer",
+                        Value = "0"
+                    });
+                }
+
+                List<string> SelectItemValues = new List<string>();
+
+                foreach (SelectListItem sli in ComputerOptions)
+                {
+                    SelectItemValues.Add(sli.Value);
+                }
+
+                // If the Employee has a computer Id assigned place that computer in the first position
+                if (!SelectItemValues.Contains(Computer.Id.ToString()))
+                {
+                    ComputerOptions.Insert(0, new SelectListItem
+                    {
+                        Text = Computer.Make,
+                        Value = Computer.Id.ToString()
+                    });
+                }
+
+                return ComputerOptions;
+            }
+        }
+
+
     }
 }
