5920d131facec6396feadf4ba011f3c396b6ab9f
Mike Parrish <40585475+mparrish615@users.noreply.github.com>
2018-11-20 10:57:35 -0600

    checkout branch

--- BangazonWorkforce/BangazonWorkforce.IntegrationTests/Config.cs
+++ BangazonWorkforce/BangazonWorkforce.IntegrationTests/Config.cs
@@ -6,7 +6,7 @@
         {
             get
             {
-                return "server=Data Source=DESKTOP-E27V8UQ\\SQLEXPRESS;Initial Catalog=BanagzonAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                return "server=DESKTOP-E27V8UQ\\SQLEXPRESS;Initial Catalog=BanagzonAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             }
         }
     }
--- BangazonWorkforce/BangazonWorkforce.IntegrationTests/DepartmentTests.cs
+++ BangazonWorkforce/BangazonWorkforce.IntegrationTests/DepartmentTests.cs
@@ -1,8 +1,14 @@
+using AngleSharp.Dom;
 using AngleSharp.Dom.Html;
 using BangazonWorkforce.IntegrationTests.Helpers;
+using BangazonWorkforce.Models;
+using Dapper;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
@@ -67,5 +73,58 @@ namespace BangazonWorkforce.IntegrationTests
                 indexPage.QuerySelectorAll("td"), 
                 td => td.TextContent.Contains(newDepartmentBudget));
         }
+
+        // This gets all the employees and passes it to Get_DeptDisplayEmployees() below.
+        private async Task<List<Employee>> AllEmployees()
+        {
+            using (IDbConnection conn = new SqlConnection(Config.ConnectionSring))
+            {
+                IEnumerable<Employee> allEmployees =
+                    await conn.QueryAsync<Employee>(@"SELECT Id, FirstName, LastName, 
+                                                              IsSupervisor, DepartmentId 
+                                                         FROM Employee
+                                                     ORDER BY Id");
+                return allEmployees.ToList();
+            }
+        }
+
+        [Fact]
+        public async Task Get_DeptDisplayEmployees()
+        {
+
+            // Arrange
+            // Creates variables to represent data to be tested
+
+            Employee employee = (await AllEmployees()).Last();
+
+            string url = $"/department/details/1";
+            string employeeFirstName = employee.FirstName;
+            string employeeLastName = employee.LastName;
+
+            // Act
+            // Gets HTTP response for data represented above
+
+            HttpResponseMessage response = await _client.GetAsync(url);
+
+
+            // Assert
+            // Checks if there is any data represented on details 
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
+
+            // Checks if data displayed represents data in database
+            IHtmlDocument detailPage = await HtmlHelpers.GetDocumentAsync(response);
+            IHtmlCollection<IElement> viewData = detailPage.QuerySelectorAll("dd");
+            Assert.Contains(
+                viewData, 
+                dd => dd.TextContent.Trim() == "Navy");
+            IHtmlCollection<IElement> lis = detailPage.QuerySelectorAll("li");
+            Assert.Contains(
+                lis, 
+                li => li.TextContent.Trim() == employee.FirstName + " " + employee.LastName);
+        }
+
     }
 }
